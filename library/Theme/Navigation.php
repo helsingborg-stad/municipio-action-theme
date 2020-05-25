548e459daba32bf3134f88c5c9dd8392330eebe8
Jacob Winding <jacobkwinding@gmail.com>
2020-05-25 13:17:17 +0200

    Remove dropdown behavior from mobile view

--- library/Theme/Navigation.php
+++ library/Theme/Navigation.php
@@ -4,20 +4,26 @@ namespace ActionHbg\Theme;
 
 class Navigation {
     public function __construct(){
-        add_filter('wp_nav_menu', array($this, 'appendDropdownClasses'), 10, 1);
+        add_filter('wp_nav_menu', array($this, 'appendDropdownClasses'), 10, 2);
     }
 
-    public function appendDropdownClasses($items)
-    {
-        $re = '/(<li.*?class=".*?menu-item-has-children.*?)(">.*?<a.*?)(>.*?<\/a>.*?<ul class="sub-menu)(".*?>.*?<\/ul>.*?<\/li>)/s';
-
+    public function appendDropdownClasses($items, $args){
         $str = $items;
+        $re = '/(<li.*?class=".*?menu-item-has-children.*?)(">.*?<a.*?)(>.*?<\/a>.*?<ul class="sub-menu)(".*?>.*?<\/ul>.*?<\/li>)/s';  
+        $replacement = '$1 js-dropdown c-dropdown c-dropdown--menu$2 class="c-dropdown__toggle"$3 unlist c-dropdown__body$4'; 
+
+        //if it's the mobile view menu, remove the sub-menu 
+        if(strpos($args->menu_class, 'mobile') !== false ) {
+            $re = '/(<li.*?class=".*?menu-item-has-children.*?>.*?)(<ul.*?class=".*?sub-menu.*?">.*?<\/ul>)/s';
+            $replacement='$1';
+        }
+
         preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
 
         if (count($matches) === 0) {
             return $items;
         }
 
-        return preg_replace($re, '$1 js-dropdown c-dropdown c-dropdown--menu$2 class="c-dropdown__toggle"$3 unlist c-dropdown__body$4', $str);
+        return preg_replace($re, $replacement, $str);
     }
 }
