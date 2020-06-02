91f0ab60ba1137bdc720736e265a3f8bb5eb8e2b
Jacob Winding <jacobkwinding@gmail.com>
2020-06-02 11:58:49 +0200

    Change color and added hover effect to show focus on a particular submenu

--- assets/source/js/megamenu.js
+++ assets/source/js/megamenu.js
@@ -3,6 +3,7 @@ export default () => {
     const mainMenuElement = document.querySelector('#main-menu'); 
     const dropdownMenus = document.querySelectorAll(".js-dropdown");
 
+
     const showAll = (event) => {
         dropdownMenus.forEach( el => { el.classList.add('is-active'); })
     }
@@ -12,5 +13,10 @@ export default () => {
 
     mainMenuElement.onmouseover = showAll;
     mainMenuElement.onmouseout = hideAll; 
+
+    for(let el of dropdownMenus){
+        el.onmouseover = (ev) => { el.classList.add("hovered"); console.log("hovered triggered") }
+        el.onmouseout = (ev) => { el.classList.remove("hovered"); }
+    }
 
   }
