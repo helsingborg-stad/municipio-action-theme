236fcf305ba09a3e14efc4744ee899c87f8391e2
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-09-14 10:16:26 +0200

    Build & introduce in animations

--- assets/source/js/app.js
+++ assets/source/js/app.js
@@ -4,7 +4,6 @@ import dropdownComponents from './dropdown';
 import megaMenu from './megamenu'; 
 import newsReel from './newsreel'; 
 
-/*
 const codeElements = document.querySelectorAll('section.section .grid [class*="grid-"], .box.box-panel ul li, .post-type-archive .breadcrumbs-wrapper + .grid .grid--columns > [class*="grid-"], .main-footer .widget_text, .main-footer .logotype, .main-footer .widget_nav_menu, .slider .slider-image > span');
 
 const observerConfig = {
@@ -18,12 +17,8 @@ const observer = new IntersectionObserver((entries) => {
             anime({
                 targets: entry.target,
                 opacity: [0,1],
-                delay: 150
-            });
-        } else {
-            anime({
-                targets: entry.target,
-                opacity: [1,0],
+                translateY: ['0','20px'],	
+                easing: 'spring(1, 80, 10, 0)', 
                 delay: 150
             });
         }
@@ -39,7 +34,6 @@ anime({
   opacity: [0,1],
   delay: anime.stagger(150, {start: 0})
 });
-*/
 
 let brickTitle = document.querySelectorAll(".home .box.box-post-brick .post-title");
 if(brickTitle.length > 0 ){
